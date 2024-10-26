c085d15103053c712a12effd80f30464d7887bf6
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-10-26 22:41:08 +0430

    Add some content to the page

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
 
 @Component({
   selector: 'app-appointment-list',
   standalone: true,
   imports: [],
   templateUrl: './appointment-list.component.html',
-  styleUrl: './appointment-list.component.css'
+  styleUrl: './appointment-list.component.css',
 })
 export class AppointmentListComponent {
-
+  appointment: Appointment = {
+    id: 1,
+    title: 'Dentist',
+    date: new Date(),
+  };
 }
